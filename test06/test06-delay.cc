a578c9e7e3d4e278e2b69bdfe7c187120832feb9
nxshen <michael7105@gmail.com>
2020-11-20 13:18:47 +0800

    test

--- test06/test06-delay.cc
+++ test06/test06-delay.cc
@@ -117,72 +117,72 @@ private:
 
 int main(int argc, char *argv[])
 {
-  string TRACE_FILE = "scratch/test06.tcl";
+  // string TRACE_FILE = "scratch/test06.tcl";
 
-  // 以下部分變數不能加上 const，因為 cmd 那邊要進行設定
-  int NODE_NUM = 100; // UE 數量
-  int BANDWIDTH = 100; // number of RB ,10MHz
-  int ENB_NUM = 35; // 設置的 eNB 數量
+  // // 以下部分變數不能加上 const，因為 cmd 那邊要進行設定
+  // int NODE_NUM = 100; // UE 數量
+  // int BANDWIDTH = 100; // number of RB ,10MHz
+  // int ENB_NUM = 35; // 設置的 eNB 數量
 
-  int SELECTED_ENB = 0; // 要測試哪一個 enb
+  // int SELECTED_ENB = 0; // 要測試哪一個 enb
 
-  double DURATION = 1000;   // 貌似是秒數
-  double ENB_TX_POWER = 20; // Transimission power in doubleBm, doubleBm 不知道是甚麼
+  // double DURATION = 1000;   // 貌似是秒數
+  // double ENB_TX_POWER = 20; // Transimission power in doubleBm, doubleBm 不知道是甚麼
 
-  Ptr<LteUePhy> uephy;
-  Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
+  // Ptr<LteUePhy> uephy;
+  // Ptr<LteHelper> lteHelper = CreateObject<LteHelper>();
 
-  Ptr<MobilityModel> ueMobilityModel;
+  // Ptr<MobilityModel> ueMobilityModel;
 
-  AsciiTraceHelper asciiTraceHelper;
-  std::ofstream outputfile1;
-  std::ofstream outputfile2;
+  // AsciiTraceHelper asciiTraceHelper;
+  // std::ofstream outputfile1;
+  // std::ofstream outputfile2;
 
-  string OUTPUT_FILE = "test06_enb0.csv";
-  string OUTPUT_FILE2 = "test06_enb1.csv";
-  string OUTPUT_DIR = "output_csv";
+  // string OUTPUT_FILE = "test06_enb0.csv";
+  // string OUTPUT_FILE2 = "test06_enb1.csv";
+  // string OUTPUT_DIR = "output_csv";
 
-  // Enable logging from the ns2 helper
-  LogComponentEnable("Ns2MobilityHelper", LOG_ALL);
+  // // Enable logging from the ns2 helper
+  // LogComponentEnable("Ns2MobilityHelper", LOG_ALL);
 
-  CommandLine cmd; // 不知道是甚麼
-  cmd.AddValue("traceFile", "Ns2 movement trace file", TRACE_FILE);
-  cmd.AddValue("nodeNum", "Number of nodes", NODE_NUM);
-  cmd.AddValue("duration", "Duration of Simulation", DURATION);
-  cmd.AddValue("selectedEnb", "Select eNB ID", SELECTED_ENB);
-  cmd.AddValue("outputDir", "output directory", OUTPUT_DIR);
-  cmd.Parse(argc, argv);
+  // CommandLine cmd; // 不知道是甚麼
+  // cmd.AddValue("traceFile", "Ns2 movement trace file", TRACE_FILE);
+  // cmd.AddValue("nodeNum", "Number of nodes", NODE_NUM);
+  // cmd.AddValue("duration", "Duration of Simulation", DURATION);
+  // cmd.AddValue("selectedEnb", "Select eNB ID", SELECTED_ENB);
+  // cmd.AddValue("outputDir", "output directory", OUTPUT_DIR);
+  // cmd.Parse(argc, argv);
 
-  char CHAR_OUTPUT_DIR[OUTPUT_DIR.length()+1];
-  strcpy(CHAR_OUTPUT_DIR, OUTPUT_DIR.c_str());
-  mkdir(CHAR_OUTPUT_DIR, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+  // char CHAR_OUTPUT_DIR[OUTPUT_DIR.length()+1];
+  // strcpy(CHAR_OUTPUT_DIR, OUTPUT_DIR.c_str());
+  // mkdir(CHAR_OUTPUT_DIR, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
-  OUTPUT_FILE = OUTPUT_DIR + "/" + to_string(SELECTED_ENB) + "_" + OUTPUT_FILE;
-  OUTPUT_FILE2 = OUTPUT_DIR + "/" + to_string(SELECTED_ENB) + "_" + OUTPUT_FILE2;
+  // OUTPUT_FILE = OUTPUT_DIR + "/" + to_string(SELECTED_ENB) + "_" + OUTPUT_FILE;
+  // OUTPUT_FILE2 = OUTPUT_DIR + "/" + to_string(SELECTED_ENB) + "_" + OUTPUT_FILE2;
 
-  outputfile1.open(OUTPUT_FILE);
-  outputfile2.open(OUTPUT_FILE2);
-  outputfile1 << "Time_sec,IMSI,SINR,X,Y,Selected_eNB" << endl;
-  outputfile2 << "Time_sec,IMSI,SINR,X,Y,Selected_eNB" << endl;
+  // outputfile1.open(OUTPUT_FILE);
+  // outputfile2.open(OUTPUT_FILE2);
+  // outputfile1 << "Time_sec,IMSI,SINR,X,Y,Selected_eNB" << endl;
+  // outputfile2 << "Time_sec,IMSI,SINR,X,Y,Selected_eNB" << endl;
 
 
-  // Set the default Configure
-  Config::SetDefault("ns3::LteEnbPhy::TxPower", DoubleValue(ENB_TX_POWER));
-  Config::SetDefault("ns3::LteUePhy::TxPower", DoubleValue(20.0)); // Transimt power in dBm
-  Config::SetDefault("ns3::LteUePhy::EnableUplinkPowerControl", BooleanValue(true));
-  Config::SetDefault("ns3::LteUePhy::EnableUplinkPowerControl", BooleanValue(true));
+  // // Set the default Configure
+  // Config::SetDefault("ns3::LteEnbPhy::TxPower", DoubleValue(ENB_TX_POWER));
+  // Config::SetDefault("ns3::LteUePhy::TxPower", DoubleValue(20.0)); // Transimt power in dBm
+  // Config::SetDefault("ns3::LteUePhy::EnableUplinkPowerControl", BooleanValue(true));
+  // Config::SetDefault("ns3::LteUePhy::EnableUplinkPowerControl", BooleanValue(true));
 
-  Config::SetDefault("ns3::LteEnbMac::NumberOfRaPreambles", UintegerValue(10));    // when too much ue will 公尛
-  Config::SetDefault("ns3::LteEnbRrc::DefaultTransmissionMode", UintegerValue(2)); // MIMO Tx diversity
+  // Config::SetDefault("ns3::LteEnbMac::NumberOfRaPreambles", UintegerValue(10));    // when too much ue will 公尛
+  // Config::SetDefault("ns3::LteEnbRrc::DefaultTransmissionMode", UintegerValue(2)); // MIMO Tx diversity
 
-  // path loss model
-  Config::SetDefault("ns3::LteHelper::PathlossModel", StringValue("ns3::OkumuraHataPropagationLossModel"));
-  Config::SetDefault("ns3::OkumuraHataPropagationLossModel::Frequency", DoubleValue(2000000000));
+  // // path loss model
+  // Config::SetDefault("ns3::LteHelper::PathlossModel", StringValue("ns3::OkumuraHataPropagationLossModel"));
+  // Config::SetDefault("ns3::OkumuraHataPropagationLossModel::Frequency", DoubleValue(2000000000));
 
-  Config::SetDefault("ns3::LteUePowerControl::ClosedLoop", BooleanValue(true));
-  Config::SetDefault("ns3::LteUePowerControl::AccumulationEnabled", BooleanValue(true));
-  Config::SetDefault("ns3::LteUePowerControl::Alpha", DoubleValue(1.0));
-  Config::SetDefault("ns3::LteEnbRrc::SrsPeriodicity", UintegerValue(320)); // he SRS periodicity in num TTIs
+  // Config::SetDefault("ns3::LteUePowerControl::ClosedLoop", BooleanValue(true));
+  // Config::SetDefault("ns3::LteUePowerControl::AccumulationEnabled", BooleanValue(true));
+  // Config::SetDefault("ns3::LteUePowerControl::Alpha", DoubleValue(1.0));
+  // Config::SetDefault("ns3::LteEnbRrc::SrsPeriodicity", UintegerValue(320)); // he SRS periodicity in num TTIs
 
   NodeContainer csmaCoreNodes;
   csmaCoreNodes.Create (10);
@@ -204,6 +204,20 @@ int main(int argc, char *argv[])
     csmaDevices[i] = csma.Install (csmaNodes[i]);
   }
 
+  NodeContainer p2pNodes[7];
+  for(int i=0;i<7;i++){
+    p2pNodes[i].Add(csmaCoreNodes.Get(i));
+    p2pNodes[i].Add(csmaNodes[i].Get(0));
+  }
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+  NetDeviceContainer p2pDevices[i];
+  for(int i=0;i<7;i++){
+    p2pDevices[i] = pointToPoint.Install (p2pNodes);
+  }
   // LteHelper lte;
   // lte.SetEnbDeviceAttribute("DlBandwidth", UintegerValue(BANDWIDTH));
   // lte.SetEnbDeviceAttribute("UlBandwidth", UintegerValue(BANDWIDTH));
@@ -287,88 +301,88 @@ int main(int argc, char *argv[])
   lteHelper->SetEnbAntennaModelType("ns3::IsotropicAntennaModel");
   lteHelper->SetEnbAntennaModelAttribute("Gain", DoubleValue(1.0));
 
-  UEs_Info *ues_info = (UEs_Info *)malloc(sizeof(UEs_Info) * NODE_NUM);
-  Ns2MobilityHelper ns2 = Ns2MobilityHelper(TRACE_FILE);
+  // UEs_Info *ues_info = (UEs_Info *)malloc(sizeof(UEs_Info) * NODE_NUM);
+  // Ns2MobilityHelper ns2 = Ns2MobilityHelper(TRACE_FILE);
 
-  // Create UE nodes.
-  NodeContainer ueNode;
-  ueNode.Create(NODE_NUM);
+  // // Create UE nodes.
+  // NodeContainer ueNode;
+  // ueNode.Create(NODE_NUM);
 
-  // configure movements for each node, while reading trace file
-  ns2.Install();
+  // // configure movements for each node, while reading trace file
+  // ns2.Install();
 
-  // Create enbNode
-  NodeContainer enbNode;
-  enbNode.Create(ENB_NUM);
+  // // Create enbNode
+  // NodeContainer enbNode;
+  // enbNode.Create(ENB_NUM);
 
-  // Set eNB position
-  Ptr<ListPositionAllocator> enbPositionAlloc = CreateObject<ListPositionAllocator>();
-  // 不知道這個 allocation 的用法，待查證
-  enbPositionAlloc->Add(Vector(500.0, 8.00, 0));
+  // // Set eNB position
+  // Ptr<ListPositionAllocator> enbPositionAlloc = CreateObject<ListPositionAllocator>();
+  // // 不知道這個 allocation 的用法，待查證
+  // enbPositionAlloc->Add(Vector(500.0, 8.00, 0));
   
-  for (int j = 1; j < 6; j++)
-  {
-    for (int i = 1; i < 8; i++)
-    {
-      Vector enbPosition(1300 + 200*i, 250 + 200*j, 0);
-      enbPositionAlloc->Add(enbPosition);
-    }
-  }
+  // for (int j = 1; j < 6; j++)
+  // {
+  //   for (int i = 1; i < 8; i++)
+  //   {
+  //     Vector enbPosition(1300 + 200*i, 250 + 200*j, 0);
+  //     enbPositionAlloc->Add(enbPosition);
+  //   }
+  // }
 
-  // Install mobility
-  MobilityHelper enbMobility;
-  enbMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-  enbMobility.SetPositionAllocator(enbPositionAlloc);
-  enbMobility.Install(enbNode);
+  // // Install mobility
+  // MobilityHelper enbMobility;
+  // enbMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+  // enbMobility.SetPositionAllocator(enbPositionAlloc);
+  // enbMobility.Install(enbNode);
 
-  // Install netdevice
-  NetDeviceContainer enbDevs;
-  NetDeviceContainer ueDevs;
-  enbDevs = lteHelper->InstallEnbDevice(enbNode);
-  ueDevs = lteHelper->InstallUeDevice(ueNode);
+  // // Install netdevice
+  // NetDeviceContainer enbDevs;
+  // NetDeviceContainer ueDevs;
+  // enbDevs = lteHelper->InstallEnbDevice(enbNode);
+  // ueDevs = lteHelper->InstallUeDevice(ueNode);
 
-  Ptr<LtePhy> enbPhy = enbDevs.Get(SELECTED_ENB)->GetObject<LteEnbNetDevice>()->GetPhy()->GetObject<LtePhy>();
+  // Ptr<LtePhy> enbPhy = enbDevs.Get(SELECTED_ENB)->GetObject<LteEnbNetDevice>()->GetPhy()->GetObject<LtePhy>();
 
-  // we install the IP stack on the UEs
-  InternetStackHelper internet1;
-  internet1.Install(ueNode);
+  // // we install the IP stack on the UEs
+  // InternetStackHelper internet1;
+  // internet1.Install(ueNode);
 
-  // Set the power density
-  Ptr<SpectrumValue> psd = enbPhy->CreateTxPowerSpectralDensity();
-  (*psd) = 4.14e-21;
-  Ptr<LteSpectrumPhy> enbDlSpectrumPhy = enbDevs.Get(SELECTED_ENB)->GetObject<LteEnbNetDevice>()->GetPhy()->GetDownlinkSpectrumPhy()->GetObject<LteSpectrumPhy>();
-  enbDlSpectrumPhy->SetNoisePowerSpectralDensity(psd);
+  // // Set the power density
+  // Ptr<SpectrumValue> psd = enbPhy->CreateTxPowerSpectralDensity();
+  // (*psd) = 4.14e-21;
+  // Ptr<LteSpectrumPhy> enbDlSpectrumPhy = enbDevs.Get(SELECTED_ENB)->GetObject<LteEnbNetDevice>()->GetPhy()->GetDownlinkSpectrumPhy()->GetObject<LteSpectrumPhy>();
+  // enbDlSpectrumPhy->SetNoisePowerSpectralDensity(psd);
 
-  // 有必要的話更換連接的 ENB ID(SELECTED_ENB)
-  for (int i = 0; i < NODE_NUM; i++)
-  {
-    lteHelper->Attach(ueDevs.Get(i), enbDevs.Get(SELECTED_ENB));
-  }
+  // // 有必要的話更換連接的 ENB ID(SELECTED_ENB)
+  // for (int i = 0; i < NODE_NUM; i++)
+  // {
+  //   lteHelper->Attach(ueDevs.Get(i), enbDevs.Get(SELECTED_ENB));
+  // }
 
-  for (int i = 0; i < NODE_NUM; i++)
-  {
-    // 從 SUMO 讀進來的路線，設置進去 ues_info[i] 裡
-    ueMobilityModel = ueNode.Get(i)->GetObject<MobilityModel>();
-    ues_info[i].set_Position(ueMobilityModel->GetPosition());
+  // for (int i = 0; i < NODE_NUM; i++)
+  // {
+  //   // 從 SUMO 讀進來的路線，設置進去 ues_info[i] 裡
+  //   ueMobilityModel = ueNode.Get(i)->GetObject<MobilityModel>();
+  //   ues_info[i].set_Position(ueMobilityModel->GetPosition());
 
-    // 不知道為什麼要 +1，應該是 eNB 的編號，車輛在 SUMO 裡應該也是 1 開始編號 // 是從 0 開始編號
-    ues_info[i].setConnectedENB(SELECTED_ENB);
-    ues_info[i].set_output(&outputfile1, &outputfile2);
+  //   // 不知道為什麼要 +1，應該是 eNB 的編號，車輛在 SUMO 裡應該也是 1 開始編號 // 是從 0 開始編號
+  //   ues_info[i].setConnectedENB(SELECTED_ENB);
+  //   ues_info[i].set_output(&outputfile1, &outputfile2);
 
-    // 車輛 IMSI，車輛編號
-    uephy = ueDevs.Get(i)->GetObject<LteUeNetDevice>()->GetPhy();
-    ues_info[i].set_imsi(ueDevs.Get(i)->GetObject<LteUeNetDevice>()->GetImsi());
+  //   // 車輛 IMSI，車輛編號
+  //   uephy = ueDevs.Get(i)->GetObject<LteUeNetDevice>()->GetPhy();
+  //   ues_info[i].set_imsi(ueDevs.Get(i)->GetObject<LteUeNetDevice>()->GetImsi());
 
-    // 設置 callback function，當 SINR 或是 position 有變動的時候，會分別呼叫這兩個 member function
-    // 請確認，SINR 變動是怎麼偵測的，是偵測 eNB 的 SINR 嗎，如果是的話，有針對哪個 eNB 嗎?
-    uephy->TraceConnectWithoutContext("ReportCurrentCellRsrpSinr", MakeCallback(&UEs_Info::GetUeSinr, &ues_info[i]));
-    ueMobilityModel->TraceConnectWithoutContext("CourseChange", MakeCallback(&UEs_Info::CourseChange1, &ues_info[i]));
-  }
+  //   // 設置 callback function，當 SINR 或是 position 有變動的時候，會分別呼叫這兩個 member function
+  //   // 請確認，SINR 變動是怎麼偵測的，是偵測 eNB 的 SINR 嗎，如果是的話，有針對哪個 eNB 嗎?
+  //   uephy->TraceConnectWithoutContext("ReportCurrentCellRsrpSinr", MakeCallback(&UEs_Info::GetUeSinr, &ues_info[i]));
+  //   ueMobilityModel->TraceConnectWithoutContext("CourseChange", MakeCallback(&UEs_Info::CourseChange1, &ues_info[i]));
+  // }
 
-  // 不知道這是啥
-  enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
-  EpsBearer bearer(q);
-  lteHelper->ActivateDataRadioBearer(ueDevs, bearer);
+  // // 不知道這是啥
+  // enum EpsBearer::Qci q = EpsBearer::GBR_CONV_VOICE;
+  // EpsBearer bearer(q);
+  // lteHelper->ActivateDataRadioBearer(ueDevs, bearer);
 
   // AnimationInterface anim("animation.xml"); // jhon 教學投影片裡的東西，也許用不到，動畫模擬之類的東西，不過 NS3 找不到
   Simulator::Stop(Seconds(DURATION));
