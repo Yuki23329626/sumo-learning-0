c5d7cf97f3d5f3b029309793e4729f02f5bc8d15
yuki23329626 <michael7105@gmail.com>
2020-12-08 09:43:22 +0800

    test

--- test.cc
+++ test.cc
@@ -28,73 +28,75 @@
 //
  
 using namespace ns3;
+using namespace std;
 
 NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
 
 int
 main (int argc, char *argv[])
 {
+    cout << "The value of x is " << x << endl;
 
-  CommandLine cmd (__FILE__);
-  cmd.Parse (argc, argv);
-  
-  Time::SetResolution (Time::NS);
-  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    CommandLine cmd (__FILE__);
+    cmd.Parse (argc, argv);
+    
+    Time::SetResolution (Time::NS);
+    LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-  NS_LOG_INFO ("Creating Topology");
+    NS_LOG_INFO ("Creating Topology");
 
-  NodeContainer nodes;
-  nodes.Create (2);
+    NodeContainer nodes;
+    nodes.Create (2);
 
-  // 拓樸輔助工具 Helper
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+    // 拓樸輔助工具 Helper
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+    pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-  // NetDeviceContainer 用來存放 NetDevice 物件
-  NetDeviceContainer devices;
-  devices = pointToPoint.Install (nodes);
+    // NetDeviceContainer 用來存放 NetDevice 物件
+    NetDeviceContainer devices;
+    devices = pointToPoint.Install (nodes);
 
-  // TCP, UDP, IP 等
-  InternetStackHelper stack;
-  stack.Install (nodes);
+    // TCP, UDP, IP 等
+    InternetStackHelper stack;
+    stack.Install (nodes);
 
-  // 為裝置分配 IP
-  Ipv4AddressHelper address;
-  // 為 10.1.1.0 的網段，subnet mask 為 255.255.255.0
-  // 預設從 10.1.1.1 開始，每次+1，下一個為 10.1.1.2
-  address.SetBase ("10.1.1.0", "255.255.255.0");
+    // 為裝置分配 IP
+    Ipv4AddressHelper address;
+    // 為 10.1.1.0 的網段，subnet mask 為 255.255.255.0
+    // 預設從 10.1.1.1 開始，每次+1，下一個為 10.1.1.2
+    address.SetBase ("10.1.1.0", "255.255.255.0");
 
-  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+    Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
-  // 一樣為設定相關參數的 helper 類物件
-  // 9 為 server port number
-  UdpEchoServerHelper echoServer (9);
+    // 一樣為設定相關參數的 helper 類物件
+    // 9 為 server port number
+    UdpEchoServerHelper echoServer (9);
 
-  // 將 container index = 1 的 container 的節點上安裝一個 UDP echo server application
-  // Install 會 return 一個 container, 包含所有被 helper 物件建立的 application pointer
-  ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
-  // 開始 跟 結束資料通訊 的時間
-  serverApps.Start (Seconds (1.0));
-  serverApps.Stop (Seconds (10.0));
+    // 將 container index = 1 的 container 的節點上安裝一個 UDP echo server application
+    // Install 會 return 一個 container, 包含所有被 helper 物件建立的 application pointer
+    ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
+    // 開始 跟 結束資料通訊 的時間
+    serverApps.Start (Seconds (1.0));
+    serverApps.Stop (Seconds (10.0));
 
-  // echo client 端相關設定
-  UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
-  // 模擬期間能傳送的 最大資料包個數
-  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
-  // 兩個 packet 間要等待多長時間
-  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-  // packet 應該包含多少資料，應該沒有包含 header
-  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+    // echo client 端相關設定
+    UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
+    // 模擬期間能傳送的 最大資料包個數
+    echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+    // 兩個 packet 間要等待多長時間
+    echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+    // packet 應該包含多少資料，應該沒有包含 header
+    echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-  // contianer index = 0
-  // 開始 與 結束時間 為 2, 10
-  ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
-  clientApps.Start (Seconds (2.0));
-  clientApps.Stop (Seconds (10.0));
+    // contianer index = 0
+    // 開始 與 結束時間 為 2, 10
+    ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+    clientApps.Start (Seconds (2.0));
+    clientApps.Stop (Seconds (10.0));
 
-  Simulator::Run ();
-  Simulator::Destroy ();
-  return 0;
+    Simulator::Run ();
+    Simulator::Destroy ();
+    return 0;
 }
