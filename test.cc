4df3f2dcb3b0683ba59c9716f691c9490913e504
yukine23329626 <michael7105@gmail.com>
2020-12-08 12:43:45 +0800

    test2

--- test.cc
+++ test.cc
@@ -16,89 +16,100 @@
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
+#include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
 
 // Default Network Topology
 //
 //       10.1.1.0
-// n0 -------------- n1
-//    point-to-point
-//
+// n0 -------------- n1   n2   n3   n4
+//    point-to-point  |    |    |    |
+//                    ================
+//                      LAN 10.1.2.0
  
 using namespace ns3;
 using namespace std;
 
-NS_LOG_COMPONENT_DEFINE ("FirstScriptExample");
+NS_LOG_COMPONENT_DEFINE ("SecondScriptExample");
 
 int
 main (int argc, char *argv[])
 {
-    cout << "The value of x is " << endl;
+    uint32_t nCsma = 3;
+    bool verbose = true;
 
-    CommandLine cmd (__FILE__);
+    CommandLine cmd;
+    cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+    cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+
     cmd.Parse (argc, argv);
-    
-    Time::SetResolution (Time::NS);
-    LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
-    NS_LOG_INFO ("Creating Topology");
+    if (verbose)
+    {
+        LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+        LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
 
-    NodeContainer nodes;
-    nodes.Create (2);
+    nCsma = nCsma == 0 ? 1 : nCsma;
 
-    // 拓樸輔助工具 Helper
+    NodeContainer p2pNodes;
+    p2pNodes.Create (2);
+
+    NodeContainer csmaNodes;
+    csmaNodes.Add (p2pNodes.Get (1));
+    csmaNodes.Create (nCsma);
+
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
     pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-    // NetDeviceContainer 用來存放 NetDevice 物件
-    NetDeviceContainer devices;
-    devices = pointToPoint.Install (nodes);
+    // 先設定 p2pDevices
+    NetDeviceContainer p2pDevices;
+    p2pDevices = pointToPoint.Install (p2pNodes);
 
-    // TCP, UDP, IP 等
+    CsmaHelper csma;
+    csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+    csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+
+    NetDeviceContainer csmaDevices;
+    csmaDevices = csma.Install (csmaNodes);
+
     InternetStackHelper stack;
-    stack.Install (nodes);
+    stack.Install (p2pNodes.Get (0));
+    stack.Install (csmaNodes);
 
-    // 為裝置分配 IP
     Ipv4AddressHelper address;
-    // 為 10.1.1.0 的網段，subnet mask 為 255.255.255.0
-    // 預設從 10.1.1.1 開始，每次+1，下一個為 10.1.1.2
     address.SetBase ("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer p2pInterfaces;
+    p2pInterfaces = address.Assign (p2pDevices);
 
-    Ipv4InterfaceContainer interfaces = address.Assign (devices);
+    address.SetBase ("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer csmaInterfaces;
+    csmaInterfaces = address.Assign (csmaDevices);
 
-    // 一樣為設定相關參數的 helper 類物件
-    // 9 為 server port number
     UdpEchoServerHelper echoServer (9);
 
-    // 將 container index = 1 的 container 的節點上安裝一個 UDP echo server application
-    // Install 會 return 一個 container, 包含所有被 helper 物件建立的 application pointer
-    ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
-    // 開始 跟 結束資料通訊 的時間
+    ApplicationContainer serverApps = echoServer.Install (csmaNodes.Get (nCsma));
     serverApps.Start (Seconds (1.0));
     serverApps.Stop (Seconds (10.0));
 
-    // echo client 端相關設定
-    UdpEchoClientHelper echoClient (interfaces.GetAddress (1), 9);
-    // 模擬期間能傳送的 最大資料包個數
+    UdpEchoClientHelper echoClient (csmaInterfaces.GetAddress (nCsma), 9);
     echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
-    // 兩個 packet 間要等待多長時間
     echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-    // packet 應該包含多少資料，應該沒有包含 header
     echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
 
-    // contianer index = 0
-    // 開始 與 結束時間 為 2, 10
-    ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+    ApplicationContainer clientApps = echoClient.Install (p2pNodes.Get (0));
     clientApps.Start (Seconds (2.0));
     clientApps.Stop (Seconds (10.0));
 
-    AsciiTraceHelper ascii;
-    pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("mytest.tr"));
-    
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+    pointToPoint.EnablePcapAll ("second");
+    csma.EnablePcap ("second", csmaDevices.Get (1), true);
+
     Simulator::Run ();
     Simulator::Destroy ();
     return 0;
