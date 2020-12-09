8c32477b948f41df052bc2bbba04b308388ba390
nxshen <michael7105@gmail.com>
2020-12-09 09:03:23 +0800

    test

--- lte_test.cc
+++ lte_test.cc
@@ -19,7 +19,7 @@
 #include <ns3/mobility-module.h>
 #include <ns3/lte-module.h>
 #include "ns3/internet-module.h"
-#include "udp-echo-helper.h"
+#include "ns3/applications-module.h"
 
 // Default Network Topology
 //
