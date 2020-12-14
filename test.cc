861c6cdb818db725b4d324061047d500953a5b33
nxshen <michael7105@gmail.com>
2020-12-14 10:25:31 +0800

    test

--- test.cc
+++ test.cc
@@ -61,8 +61,8 @@ int main (int argc, char *argv[])
     NodeContainer p2pNodes;
     p2pNodes.Create (2);
 
-    NodeContainer p2pNodes2;
-    p2pNodes2.Create (2);
+    // NodeContainer p2pNodes2;
+    // p2pNodes2.Create (2);
 
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
@@ -71,12 +71,12 @@ int main (int argc, char *argv[])
     NetDeviceContainer p2pDevices;
     p2pDevices = pointToPoint.Install (p2pNodes);
 
-    NetDeviceContainer p2pDevices2;
-    p2pDevices2 = pointToPoint.Install (p2pNodes2);
+    // NetDeviceContainer p2pDevices2;
+    // p2pDevices2 = pointToPoint.Install (p2pNodes2);
 
     NodeContainer csmaNodes;
     csmaNodes.Add (p2pNodes.Get (1));
-    csmaNodes.Add (p2pNodes2.Get (1));
+    // csmaNodes.Add (p2pNodes2.Get (1));
     csmaNodes.Create (nCsma);
 
     CsmaHelper csma;
@@ -91,7 +91,7 @@ int main (int argc, char *argv[])
     wifiStaNodes.Create (nWifi);
 
     NodeContainer wifiApNode = p2pNodes.Get (0);
-    NodeContainer wifiApNode2 = p2pNodes2.Get (0);
+    // NodeContainer wifiApNode2 = p2pNodes2.Get (0);
 
     YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
     YansWifiPhyHelper phy = YansWifiPhyHelper::Default ();
@@ -117,8 +117,8 @@ int main (int argc, char *argv[])
     NetDeviceContainer apDevices;
     apDevices = wifi.Install (phy, mac, wifiApNode);
 
-    NetDeviceContainer apDevices2;
-    apDevices2 = wifi.Install (phy, mac, wifiApNode2);
+    // NetDeviceContainer apDevices2;
+    // apDevices2 = wifi.Install (phy, mac, wifiApNode2);
 
     // wifi sta mobility
     MobilityHelper mobility;
@@ -139,14 +139,14 @@ int main (int argc, char *argv[])
     mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
     mobility.Install (wifiApNode);
 
-    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-    mobility.Install (wifiApNode2);
+    // mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+    // mobility.Install (wifiApNode2);
 
     // internet settings
     InternetStackHelper stack;
     stack.Install (csmaNodes);
     stack.Install (wifiApNode);
-    stack.Install (wifiApNode2);
+    // stack.Install (wifiApNode2);
     stack.Install (wifiStaNodes);
 
     Ipv4AddressHelper address;
@@ -162,7 +162,7 @@ int main (int argc, char *argv[])
     address.SetBase ("10.1.3.0", "255.255.255.0");
     address.Assign (staDevices);
     address.Assign (apDevices);
-    address.Assign (apDevices2);
+    // address.Assign (apDevices2);
 
     UdpEchoServerHelper echoServer (9);
 
