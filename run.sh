20ae63e65dfbc69e33de3eafc38b569c2206b32d
nxshen <michael7105@gmail.com>
2021-01-06 03:36:27 +0000

    update

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ echo "cp ./ns3lte/lteudp_test1.cc ../scratch/"
 cp ./ns3lte/lteudp_test1.cc ../scratch/
 echo "cp ./test09/test09.tcl ../scratch/"
 cp ./test09/test09.tcl ../scratch/
-echo "cp ./test09/2.png ../scratch/"
-cp ./test09/2.png ../scratch/
+echo "cp ./test09/background.png ../scratch/"
+cp ./test09/background.png ../scratch/
 cd ..
 cd ..
 echo "./waf --run "scratch/lteudp_test1""
