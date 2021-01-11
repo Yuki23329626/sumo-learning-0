fc219a6952e6aa802e5dcf32283d7b5e8808723d
nxshen <michael7105@gmail.com>
2021-01-11 23:38:23 +0800

    test

--- ns3lte/lteudp_test1.cc
+++ ns3lte/lteudp_test1.cc
@@ -172,7 +172,7 @@ int main (int argc, char *argv[])
 
 	// EnodeB = 3, UE = 20
 	string TRACE_FILE = "scratch/test09.tcl";
-	uint16_t numberOfNodesENB = 10;
+	uint16_t numberOfNodesENB = 3;
 	uint16_t numberOfNodesEU = 100;
 	double simTime = 0.05;
 	double distance = 250.0;
