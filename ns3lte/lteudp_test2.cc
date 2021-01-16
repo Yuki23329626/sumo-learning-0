f324ca645665f8bada5660031443b614cd4c080e
nxshen <michael7105@gmail.com>
2021-01-17 00:09:43 +0800

    test

--- ns3lte/lteudp_test2.cc
+++ ns3lte/lteudp_test2.cc
@@ -299,9 +299,9 @@
     enbMobility.Install (enbNodes);
   
     // Install Mobility Model in UE
-    // MobilityHelper ueMobility;
-    // ueMobility.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
-    // ueMobility.Install (ueNodes);
+    MobilityHelper ueMobility;
+    ueMobility.SetMobilityModel ("ns3::ConstantVelocityMobilityModel");
+    ueMobility.Install (ueNodes);
     // // ueNodes.Get (0)->GetObject<MobilityModel> ()->SetPosition (Vector (0, yForUe, 0));
     // ueNodes.Get (0)->GetObject<MobilityModel> ()->SetPosition (Vector (583, 365, 0));
     // ueNodes.Get (0)->GetObject<ConstantVelocityMobilityModel> ()->SetVelocity (Vector (734/60, 585/60, 0));
