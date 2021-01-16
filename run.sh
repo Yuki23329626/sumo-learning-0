dca14e407645075cb7c2cfda4068bf7e393b369a
nxshen <michael7105@gmail.com>
2021-01-16 23:12:47 +0800

    test2

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 cd sumo-learning
 git pull
-echo "cp ./ns3lte/lteudp_test1.cc ../scratch/"
-cp ./ns3lte/lteudp_test1.cc ../scratch/
+echo "cp ./ns3lte/lteudp_test2.cc ../scratch/"
+cp ./ns3lte/lteudp_test2.cc ../scratch/
 echo "cp ./test10/test10.tcl ../scratch/"
 cp ./test10/test10.tcl ../scratch/
 echo "cp ./test10/background.png ../scratch/"
 cp ./test10/background.png ../scratch/
 cd ..
-echo "./waf --run \"scratch/lteudp_test1\""
-./waf --run "scratch/lteudp_test1"
+echo "./waf --run \"scratch/lteudp_test2\""
+./waf --run "scratch/lteudp_test2"
