bdea6dca74f08c4fed7403658910ba57354eda86
nxshen <michael7105@gmail.com>
2021-01-17 02:00:26 +0800

    test

--- ns3lte/lteudp_test2.cc
+++ ns3lte/lteudp_test2.cc
@@ -190,6 +190,7 @@
     Config::SetDefault ("ns3::UdpClient::Interval", TimeValue (MilliSeconds (10)));
     Config::SetDefault ("ns3::UdpClient::MaxPackets", UintegerValue (1000000));
     Config::SetDefault ("ns3::LteHelper::UseIdealRrc", BooleanValue (true));
+    Config::SetDefault ("ns3::LteEnbMac::NumberOfRaPreamables", UintegerValue (10)); 
   
     // Command line arguments
     CommandLine cmd (__FILE__);
