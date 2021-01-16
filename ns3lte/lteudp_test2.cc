9825c1661c5c2e40312129d32ee60400b025d155
nxshen <michael7105@gmail.com>
2021-01-16 23:44:32 +0800

    debug

--- ns3lte/lteudp_test2.cc
+++ ns3lte/lteudp_test2.cc
@@ -303,7 +303,7 @@
     // // ueNodes.Get (0)->GetObject<MobilityModel> ()->SetPosition (Vector (0, yForUe, 0));
     // ueNodes.Get (0)->GetObject<MobilityModel> ()->SetPosition (Vector (583, 365, 0));
     // ueNodes.Get (0)->GetObject<ConstantVelocityMobilityModel> ()->SetVelocity (Vector (734/60, 585/60, 0));
-    ns2.install(ueNodes)
+    ns2.Install(ueNodes)
   
     // Install LTE Devices in eNB and UEs
     Config::SetDefault ("ns3::LteEnbPhy::TxPower", DoubleValue (enbTxPowerDbm));
