58d9ce85c1d49f8bf4795a1243ffcbb443364fb4
nxshen <michael7105@gmail.com>
2021-01-21 02:17:20 +0800

    test

--- run.sh
+++ run.sh
@@ -6,8 +6,10 @@ echo "cp ./ns3lte/lteudp_test2.cc ../scratch/"
 cp ./ns3lte/lteudp_test2.cc ../scratch/
 echo "cp ./test10/test10.tcl ../scratch/"
 cp ./test10/test10.tcl ../scratch/
-echo "cp ./test10/background.png ../scratch/"
-cp ./test10/background.png ../scratch/
+echo "cp ./test10/oneUE.tcl ../scratch/"
+cp ./test10/oneUE.tcl ../scratch/
+# echo "cp ./test10/background.png ../scratch/"
+# cp ./test10/background.png ../scratch/
 cd ..
 echo "./waf --run \"scratch/lteudp_test2\""
 ./waf --run "scratch/lteudp_test2"
