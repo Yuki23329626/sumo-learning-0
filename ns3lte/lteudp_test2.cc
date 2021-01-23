6d75f5b80d8aa0aa86f0f5c7cd40dd375eb02715
nxshen <michael7105@gmail.com>
2021-01-24 07:30:47 +0800

    test

--- ns3lte/lteudp_test2.cc
+++ ns3lte/lteudp_test2.cc
@@ -230,7 +230,7 @@ void attachToClosestEnb(NodeContainer* ueNodes, NetDeviceContainer* ueLteDevs, N
         std::cout << "enb:" << index << ", distance: " << min_distance << std::endl;
       }
     }
-    lteHelper->Attach (ueLteDevs->Get(i), enbLteDevs->Get(index));
+    lteHelper->AttachToClosestEnb (ueLteDevs->Get(i), enbLteDevs->Get(index));
   }
 }
   
