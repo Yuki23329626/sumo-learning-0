31fc46d1689e219365dcf199ce6005976fae3a74
nxshen <michael7105@gmail.com>
2021-01-24 19:02:00 +0800

    test

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 cd sumo-learning
 git pull
 echo "cp ./ns3lte/lteudp_test3.cc ../scratch/"
-cp ./ns3lte/lteudp_test2.cc ../scratch/
+cp ./ns3lte/lteudp_test3.cc ../scratch/
 echo "cp ./test10/test10.tcl ../scratch/"
 cp ./test10/test10.tcl ../scratch/
 echo "cp ./test10/oneUE.tcl ../scratch/"
