8da52981b1a915afcab6fbf9166c00dbb0a7685d
nxshen <michael7105@gmail.com>
2021-01-25 01:21:13 +0800

    test

--- ns3lte/lteudp_test3.cc
+++ ns3lte/lteudp_test3.cc
@@ -431,38 +431,38 @@ int main (int argc, char *argv[])
   //     lteHelper->Attach (ueLteDevs.Get (i), enbLteDevs.Get (0));
   //   }
 
-  uint16_t j = 0;
-  for (uint16_t i = 0; i < numberOfUes; i++){  
-    if (j < numberOfEnbs){
-        lteHelper->Attach (ueLteDevs.Get(i), enbLteDevs.Get(j));
-        last_index[i] = j;
-        j++;
-    }
-    else{
-        j = 0;
-        lteHelper->Attach (ueLteDevs.Get(i), enbLteDevs.Get(j));
-        last_index[i] = j;
-    }   
-  }
-
-  // for(int i=0; i<numberOfUes; i++){
-  //   Ptr<const MobilityModel> ueMobilityModel = ueNodes.Get(i)->GetObject<MobilityModel>();
-  //   Vector pos_ue = ueMobilityModel->GetPosition ();
-  //   int index = -1;
-  //   unsigned long long int min_distance = std::numeric_limits<int>::max();
-  //   for(int j=0; j<numberOfEnbs; j++){
-  //     Ptr<const MobilityModel> enbMobilityModel = enbNodes.Get(j)->GetObject<MobilityModel>();
-  //     Vector pos_enb = enbMobilityModel->GetPosition ();
-  //     float current_distance = sqrt((pos_ue.x-pos_enb.x)*(pos_ue.x-pos_enb.x)+(pos_ue.y-pos_enb.y)*(pos_ue.y-pos_enb.y));
-  //     if( current_distance < min_distance ){
-  //       min_distance = current_distance;
-  //       index = j;
-  //       std::cout << "ue: " << i <<  ", enb: " << index << ", distance: " << min_distance << endl << endl;
-  //     }
+  // uint16_t j = 0;
+  // for (uint16_t i = 0; i < numberOfUes; i++){  
+  //   if (j < numberOfEnbs){
+  //       lteHelper->Attach (ueLteDevs.Get(i), enbLteDevs.Get(j));
+  //       last_index[i] = j;
+  //       j++;
   //   }
-  //   lteHelper->AttachToClosestEnb (ueLteDevs.Get(i), enbLteDevs.Get(index));
-  //   last_index[i] = index;
+  //   else{
+  //       j = 0;
+  //       lteHelper->Attach (ueLteDevs.Get(i), enbLteDevs.Get(j));
+  //       last_index[i] = j;
+  //   }   
   // }
+
+  for(int i=0; i<numberOfUes; i++){
+    Ptr<const MobilityModel> ueMobilityModel = ueNodes.Get(i)->GetObject<MobilityModel>();
+    Vector pos_ue = ueMobilityModel->GetPosition ();
+    int index = -1;
+    unsigned long long int min_distance = std::numeric_limits<int>::max();
+    for(int j=0; j<numberOfEnbs; j++){
+      Ptr<const MobilityModel> enbMobilityModel = enbNodes.Get(j)->GetObject<MobilityModel>();
+      Vector pos_enb = enbMobilityModel->GetPosition ();
+      float current_distance = sqrt((pos_ue.x-pos_enb.x)*(pos_ue.x-pos_enb.x)+(pos_ue.y-pos_enb.y)*(pos_ue.y-pos_enb.y));
+      if( current_distance < min_distance ){
+        min_distance = current_distance;
+        index = j;
+        std::cout << "ue: " << i <<  ", enb: " << index << ", distance: " << min_distance << endl << endl;
+      }
+    }
+    lteHelper->AttachToClosestEnb (ueLteDevs.Get(i), enbLteDevs.Get(index));
+    last_index[i] = index;
+  }
 
 
   NS_LOG_LOGIC ("setting up applications");
