0f2d8df8a67a5cc06505ea3019ea9788eafd9146
nxshen <michael7105@gmail.com>
2021-01-24 08:16:44 +0800

    test

--- ns3lte/lteudp_test2.cc
+++ ns3lte/lteudp_test2.cc
@@ -231,7 +231,7 @@ void attachToClosestEnb(NodeContainer* ueNodes, NetDeviceContainer* ueLteDevs, N
       }
     }
     if(last_index[i] != index){
-      cout << "last_index: " << i << ", index: " << j << endl;
+      cout << "last_index: " << last_index[i] << ", index: " << i << endl;
       lteHelper->HandoverRequest(Seconds(Simulator::Now ().GetSeconds()+0.100), ueLteDevs->Get(i), enbLteDevs->Get(last_index[i]), enbLteDevs->Get(index));
       last_index[i] = index;
     }
