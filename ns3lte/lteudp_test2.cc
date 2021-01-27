60a19f9b51ef863d7ea68d9c8de970cf8780723c
yukine23329626 <michael7105@gmail.com>
2021-01-27 15:34:32 +0800

    test

--- ns3lte/lteudp_test2.cc
+++ ns3lte/lteudp_test2.cc
@@ -311,6 +311,7 @@ int main (int argc, char *argv[])
   cmd.AddValue("distance", "Distance between eNBs [m]", distance);
   cmd.AddValue("interPacketInterval", "Inter packet interval [ms])", interPacketInterval);
   cmd.AddValue ("animFile",  "File Name for Animation Output", animFile);
+  cmd.AddValue ("traceFile",  "File Name for Trace Input", traceFile);
 
   cmd.Parse (argc, argv);
 
