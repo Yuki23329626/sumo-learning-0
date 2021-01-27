b7be8b1a0028e4e8dfbb96c7d5dfa3d23d6991f3
yukine23329626 <michael7105@gmail.com>
2021-01-27 17:08:10 +0800

    test

--- log_to_csv.py
+++ log_to_csv.py
@@ -3,12 +3,12 @@ import csv
 data = open("log.lteudp_test2-2", 'r')
 lines = data.readlines()
 
-for line in lines:
-    string1 = line.split(" ")
-    if(string1[0] == "TraceDelay:"):
-        list1 = list(string1[16][1:-3])
-        print(string1[16][1:-3])
-        with open("lteudp_test2-2.csv", 'w', newline="") as csvfile:
+with open("lteudp_test2-2.csv", 'w') as csvfile:
+    for line in lines:
+        string1 = line.split(" ")
+        if(string1[0] == "TraceDelay:"):
+            list1 = list(string1[16][1:-3].split(" "))
+            print(list1)
             writer = csv.writer(csvfile)
             writer.writerow(list1)
-
+            break
