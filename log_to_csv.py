1242e2ed8b6495ff76f363d43b0c0c0544d55cbe
yukine23329626 <michael7105@gmail.com>
2021-01-27 17:09:42 +0800

    test

--- log_to_csv.py
+++ log_to_csv.py
@@ -3,12 +3,14 @@ import csv
 data = open("log.lteudp_test2-2", 'r')
 lines = data.readlines()
 
+count = 0
 with open("lteudp_test2-2.csv", 'w') as csvfile:
     for line in lines:
+        if(count == 10) break
         string1 = line.split(" ")
         if(string1[0] == "TraceDelay:"):
             list1 = list(string1[16][1:-3].split(" "))
             print(list1)
             writer = csv.writer(csvfile)
             writer.writerow(list1)
-            break
+            count = count + 1
