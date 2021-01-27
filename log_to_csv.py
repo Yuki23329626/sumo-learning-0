215268bc6367a79f94b670f66b9d7df03ab20d0f
yukine23329626 <michael7105@gmail.com>
2021-01-27 16:57:38 +0800

    test

--- log_to_csv.py
+++ log_to_csv.py
@@ -1,3 +1,4 @@
+import csv
 
 data = open("log.lteudp_test2-2", 'r')
 lines = data.readlines()
@@ -5,6 +6,9 @@ lines = data.readlines()
 for line in lines:
     string1 = line.split(" ")
     if(string1[0] == "TraceDelay:"):
-        print(string1[16])
-        break
+        list1 = list(string1[16][1:-3])
+        print(string1[16][1:-3])
+        with open("lteudp_test2-2.csv", 'w', newline="") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(list1)
 
