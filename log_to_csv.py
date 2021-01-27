b5f29c0d2d959bfac8e814bf1ab11eddce447c2f
nxshen <michael7105@gmail.com>
2021-01-27 19:26:03 +0000

    update

--- log_to_csv.py
+++ log_to_csv.py
@@ -1,12 +1,21 @@
 import csv
+import sys
+import shlex
+import os
+filename = " ".join(map(shlex.quote, sys.argv[1:]))
+print("filename: ", filename)
 
-data = open("log.lteudp_test2", 'r')
+if(filename == "" or not os.path.exists(filename)):
+    print("please type the correct file name of the log")
+    exit()
+
+data = open(filename, 'r')
 lines = data.readlines()
 
 count_sent = 0
 count = 0
 sum = 0
-with open("lteudp_test2.csv", 'w') as csvfile:
+with open("csv_"+filename+".csv", 'w') as csvfile:
     for line in lines:
         #if(count == 10):
         #    break
@@ -21,6 +30,6 @@ with open("lteudp_test2.csv", 'w') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(list1)
             count = count + 1
-print("count_sent: ", count_sent/2)
+print("count_sent: ", count_sent)
 print("count: ", count)
-print("average delay(ms): ", sum/count)
+print("average delay(s): ", sum/count)
