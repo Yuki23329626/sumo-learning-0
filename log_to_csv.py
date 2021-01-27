dc90e3cbfa5ec634793920e101e8169bbbd2d6d1
yukine23329626 <michael7105@gmail.com>
2021-01-27 16:46:32 +0800

    test

--- log_to_csv.py
+++ log_to_csv.py
@@ -3,5 +3,8 @@ data = open("log.lteudp_test2-2", 'r')
 lines = data.readlines()
 
 for line in lines:
-    test = line.split(" ")[0]
-    print(test)
+    string1 = line.split(" ")
+    if(string1[0] == "TraceDelay:":
+        print(string1[16])
+        break
+
