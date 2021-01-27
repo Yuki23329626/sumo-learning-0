dc9d3eb8f3ef9ec69d731582db4c5734acf8c5b7
nxshen <michael7105@gmail.com>
2021-01-27 22:36:00 +0800

    test

--- log_to_csv.py
+++ log_to_csv.py
@@ -3,15 +3,20 @@ import csv
 data = open("log.lteudp_test2-2", 'r')
 lines = data.readlines()
 
-#count = 0
+count = 0
+sum = 0
 with open("lteudp_test2-2.csv", 'w') as csvfile:
     for line in lines:
         #if(count == 10):
         #    break
         string1 = line.split(" ")
         if(string1[0] == "TraceDelay:"):
-            list1 = list(str(eval(string1[16][1:-3])/1000000000).split(" "))
+            floatDelay = eval(string1[16][1:-3])/1000000000
+            sum = sum + floatDelay
+            list1 = list(str(floatDelay).split(" "))
             # print(str(eval(string1[16][1:-3])/1000000000))
             writer = csv.writer(csvfile)
             writer.writerow(list1)
-            #count = count + 1
+            count = count + 1
+print("count: ", count)
+print("average delay(ms): ", sum/count)
