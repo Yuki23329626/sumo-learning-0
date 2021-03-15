adfa47ee431359ebf6a4845eed69b606889a3fa3
nxshen <michael7105@gmail.com>
2021-03-15 13:08:26 +0800

    test

--- ns3lte/lteudp_test4.cc
+++ ns3lte/lteudp_test4.cc
@@ -369,7 +369,7 @@ int main (int argc, char *argv[])
     double enbTxPowerDbm = 46.0;
     double interPacketInterval = 1000.0;
     double interAppInterval = 10.0; // sec
-    uint16_t sdnInterval = 100; // millisecond
+    uint16_t sdnInterval = 200; // millisecond
     uint16_t nMaxPackets = 1024;
     uint16_t nPayloadBytes = 1024;
     isSdnEnabled = true;
