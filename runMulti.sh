e187550093a5235857249440404b3f3babfd9684
nxshen <michael7105@gmail.com>
2021-03-16 00:59:34 +0800

    test

--- runMulti.sh
+++ runMulti.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+trap "kill 0" INT
+
 cd sumo-learning
 git pull
 cp ./ns3lte/lteudpMulti.cc ../scratch/
 cp ./test10/test10.tcl ../scratch/
 cd ..
-(./waf --run "scratch/lteudpMulti --numberOfUes=10 --startUe=0 --endUe=5 --isSdnEnabled=true --isDownlink=true --isUplink=true" > log.lteudpMulti 2>&1) &
-(./waf --run "scratch/lteudpMulti --numberOfUes=10 --startUe=5 --endUe=10 --isSdnEnabled=true --isDownlink=true --isUplink=true" > log.lteudpMulti 2>&1) &
+(./waf --run "scratch/lteudpMulti --numberOfUes=10 --startUe=0 --endUe=5 --isSdnEnabled=true --isDownlink=true --isUplink=true" > log.lteudpMultiPart0 2>&1) &
+(./waf --run "scratch/lteudpMulti --numberOfUes=10 --startUe=5 --endUe=10 --isSdnEnabled=true --isDownlink=true --isUplink=true" > log.lteudpMultiPart5 2>&1) &
+
+wait
