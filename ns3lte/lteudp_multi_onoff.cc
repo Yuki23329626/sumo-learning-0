31304066cca5c2e6008f4262d0cc4377390246c1
nxshen <michael7105@gmail.com>
2021-03-17 00:55:32 +0800

    test

--- ns3lte/lteudp_multi_onoff.cc
+++ ns3lte/lteudp_multi_onoff.cc
@@ -797,10 +797,11 @@ for (uint32_t u = startUe; u < endUe; ++u){
                     MakeCallback (&NotifyHandoverEndOkUe));
     // Config::Connect ("/NodeList/*/$ns3::MobilityModel/CourseChange",
     //                 MakeCallback (&CourseChange));
-    // Config::Connect ("/NodeList/*/ApplicationList/*/$ns3::OnOffApplication/TxWithAddresses",
-    //                 MakeCallback (&TxTrace));
-    // Config::Connect ("/NodeList/*/ApplicationList/*/$ns3::PacketSink/RxWithAddresses",
-    //                 MakeCallback (&RxTrace));
+
+    Config::Connect ("/NodeList/*/ApplicationList/*/$ns3::OnOffApplication/TxWithAddresses",
+                    MakeCallback (&TxTrace));
+    Config::Connect ("/NodeList/*/ApplicationList/*/$ns3::PacketSink/RxWithAddresses",
+                    MakeCallback (&RxTrace));
 
 //   // uncomment to enable anim
 //   // Create the animation object and configure for specific output
