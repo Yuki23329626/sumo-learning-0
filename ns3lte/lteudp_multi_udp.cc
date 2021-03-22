7202664b849501a200c4fcc742e60e38195375ad
nxshen <michael7105@gmail.com>
2021-03-22 18:56:20 +0800

    test

--- ns3lte/lteudp_multi_udp.cc
+++ ns3lte/lteudp_multi_udp.cc
@@ -366,7 +366,7 @@ int main (int argc, char *argv[])
     double distance = 500.0; // m
     double speed = 20;       // m/s
     double simTime = 60; // 1500 m / 20 m/s = 75 secs
-    double enbTxPowerDbm = 46.0;
+    double enbTxPowerDbm = 24.0;
     double interPacketInterval = 1000.0;
     double interAppInterval = 10.0; // sec
     uint16_t startUe = 0;
